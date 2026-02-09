a6ae01b9797eb6c2018ffc65c981908724f9b5b0
PaleAvery <143568240+PaleAvery@users.noreply.github.com>
2026-02-10 11:56:01 +1300

    Add files via upload
    created inefficient temp gui this can display as a window pretty neat but a lot of improvement for the future

---
+++ C_01_Temp_Gui_v1.py
@@ -0,0 +1,68 @@
+from tkinter import *
+
+class Converter():
+    """
+    Temperature conversion tool (C to F or F to C )
+    """
+
+    def __init__(self):
+        """
+        Temperature converter gui
+        """
+
+        self.temp_frame = Frame(padx=10, pady=10)
+        self.temp_frame.grid()
+
+        self.temp_heading = Label(self.temp_frame,
+                                  text ="Temperature Converter",
+                                  font=("Arial", "16", "bold")
+                                  )
+        self.temp_heading.grid(row=0)
+
+        instructions = ("Please enter a temperature below and then press one of the "
+                        "buttons to convert ut from centrigrade "
+                        "to farenheit.")
+        self.temp_instructions = Label(self.temp_frame,
+                                       text=instructions,
+                                       wraplength=150, width=40,
+                                       justify="left")
+        self.temp_instructions.grid(row=1)
+
+        self.temp_entry = Entry(self.temp_frame,
+                                font=("Arail","14")
+                                )
+        self.temp_entry.grid(row=2, padx=10,pady=10)
+
+        error = "please enter a number "
+        self.temp_error = Label(self.temp_frame, text=error,
+                                fg="#9C0000")
+        self.temp_error.grid(row=3)
+
+        # conversion help and history / export buttons
+        self.button_frame = Frame(self.temp_frame)
+        self.button_frame.grid(row=4)
+
+        self.to_celsius_button = Button(self.button_frame,
+                                        text="To Celsius",
+                                        bg="#990099",
+                                        fg="#ffffff",
+                                        font=("Arial","12","bold"), width=12)
+        self.to_celsius_button.grid(row=0, column=0, padx=5, pady=5)
+
+        self.to_fahrenheit_button = Button(self.button_frame,
+                                        text="To Fahrenheit",
+                                        bg="#009900",
+                                        fg="#ffffff",
+                                        font=("Arial","12","bold"), width=12)
+        self.to_fahrenheit_button.grid(row=0, column=1, padx=5, pady=5)
+
+
+
+    # main routine
+
+
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Temperature converter")
+    Converter()
+    root.mainloop()
