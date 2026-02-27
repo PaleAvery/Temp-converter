5d2bd194d7e945a34b33aa25bd81132f5922e19e
PaleAvery <143568240+PaleAvery@users.noreply.github.com>
2026-02-27 13:15:17 +1300

    working hints upgraded base

--- B_01_Colour_Quest_v1.py
+++ B_01_Colour_Quest_v1.py
@@ -51,8 +51,9 @@ def get_round_colours():
     int_scores.sort()
     median = (int_scores[1] + int_scores[2]) / 2
     median = round_ans(median)
+    highest = int_scores[-1]
 
-    return round_colours, median
+    return round_colours,median,highest
 
 def round_ans(val):
     """
@@ -187,6 +188,7 @@ class Play:
         self.round_colour_list = []
         self.all_scores_list = []
         self.all_medians_list = []
+        self.all_high_score_list = []
 
         self.play_box = Toplevel()
 
@@ -280,10 +282,14 @@ class Play:
         rounds_wanted = self.rounds_wanted.get()
 
         # get round colours and median score...
-        self.round_colour_list, median = get_round_colours()
+        self.round_colour_list, median,highest = get_round_colours()
 
         # Set target score as median (for later comparison)
         self.target_score.set(median)
+
+        # add median and high scorre to listsd for stats
+        self.all_medians_list.append(median)
+        self.all_high_score_list.append(highest)
 
         # Update heading, and score to beat labels.  "Hide" results label
         self.heading_label.config(text=f"Round {rounds_played} of {rounds_wanted}")
@@ -327,6 +333,11 @@ class Play:
             self.all_scores_list.append(0)
 
         self.results_label.config(text=result_text, bg=result_bg)
+
+        #printing are ti generate test data for stats
+        print("all scores", self.all_scores_list)
+        print("all median", self.all_medians_list)
+        print("highest scores", self.all_high_score_list)
 
         # enable stats & next buttons,  disable colour buttons
         self.next_button.config(state=NORMAL)
